22968fdc88bd9e39c819d8e4db5fcf16de031f71
barrycade11 <43128329+barrycade11@users.noreply.github.com>
2025-04-07 20:56:17 +0800

    Update ApiClient.js

--- fuel_management/app/Constants/ApiClient.js
+++ fuel_management/app/Constants/ApiClient.js
@@ -1,13 +1,13 @@
 import axios from "axios";
 import useAuth from "~/Hooks/Auth/useAuth";
 
-const API_BASE_URL = import.meta.env.API_BASE_URL;
-const API_PORT = import.meta.env.API_PORT;
+const API_BASE_URL = import.meta.env.VITE_API_BASE_URL;
+const API_PORT = import.meta.env.VITE_API_PORT;
 
 const { token } = useAuth.getState();
 
 const apiClient = axios.create({
-  baseURL: `${API_BASE_URL}:${API_PORT}`,
+  baseURL: `http://${API_BASE_URL}:${API_PORT}`,
   headers: {
     "Content-Type": "application/json",
     "Authorization": `Bearer ${token}`,
